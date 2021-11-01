965e372db97b36924e2a6d3c6125063b1162ef1e
YiiXiii2 <yinxuann1357@gmail.com>
2021-11-01 21:21:11 +0800

    cookbook3

---
+++ cookbook3/app/models/recipe.rb
@@ -0,0 +1,3 @@
+class Recipe < ApplicationRecord
+	belongs_to :category, optional:true
+end
---
+++ cookbook3/db/migrate/20211101131059_add_instruction_to_recipe.rb
@@ -0,0 +1,5 @@
+class AddInstructionToRecipe < ActiveRecord::Migration[6.1]
+  def change
+    add_column :recipes, :instructions, :string
+  end
+end
