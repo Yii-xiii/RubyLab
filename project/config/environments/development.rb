ced47d02e9f34b6e1b89048d88252905dca56c92
Xuann <yinxuann1357@gmail.com>
2021-12-28 12:29:31 +0800

    added config hosts

--- project/config/environments/development.rb
+++ project/config/environments/development.rb
@@ -1,6 +1,8 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.hosts << "ruby-git.act.buaa.edu.cn"
+  config.action_mailer.default_url_options = { host: 'ruby-git.act.buaa.edu.cn', port:5000 }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
